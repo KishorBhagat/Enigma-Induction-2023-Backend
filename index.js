f1cab9e6caef14b7847307f8629a008ec404d9e5
Kishor Bhagat <kishorebhagat663@gmail.com>
2023-03-13 22:44:23 +0530

    route update

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 
 app.use(cors());
-app.use('/api/loc', authRoute);
+app.use('/api/induction', authRoute);
 
 app.listen(port, ()=>{
     console.log("Server listening at port " + port);
